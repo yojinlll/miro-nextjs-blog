f136c8e6134be566215551c3081a9be4a20aa51c
yojin <yojin1995@qq.com>
2020-10-17 00:20:24 +0800

    pg test

--- dist/seed.js
+++ dist/seed.js
@@ -10,16 +10,39 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var posts;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log('connection', connection);
-            connection.close();
+            _context.next = 2;
+            return connection.manager.find(_Post.Post);
 
           case 2:
+            posts = _context.sent;
+
+            if (!(posts.length === 0)) {
+              _context.next = 6;
+              break;
+            }
+
+            _context.next = 6;
+            return connection.manager.save([1, 2, 3, 4, 5].map(function (i) {
+              return new _Post.Post({
+                title: "first md ".concat(i, "--"),
+                content: "first md ".concat(i, " content--")
+              });
+            }));
+
+          case 6:
+            console.log('-posts', posts);
+            connection.close();
+
+          case 8:
           case "end":
             return _context.stop();
         }
