5c0cae179d1d3735beb7e054999400f5772ee875
yojin <yojin1995@qq.com>
2020-10-18 23:01:13 +0800

    获取 & 展示数据

--- pages/SSR_index.tsx
+++ pages/SSR_index.tsx
@@ -1,9 +1,11 @@
 import { getDatabaseConnection } from 'lib/getDatabaseConnection';
 import { GetServerSideProps, NextPage } from 'next'
+import { Post } from 'src/entity/Post';
 // import Link from 'next/link'
 import { UAParser } from "ua-parser-js";
 
 type Props = {
+  posts: Post[]
   browser: {
     major: string
     name: string
@@ -12,13 +14,23 @@ type Props = {
 }
 
 const SSRIndex: NextPage<Props> = (props) => {
-  console.log(props.browser);
+  console.log('props.post', props.posts);
   
   return (
     <div>
       <h1>SSR test</h1>
 
       <h2>你的浏览器是 { JSON.stringify(props.browser) }</h2>
+
+      <div>
+        {
+          props.posts.map(post => {
+            return <div key={post.id}>
+              {post.title}
+            </div>
+          })
+        }
+      </div>
     </div>
   )
 }
@@ -27,14 +39,17 @@ export default SSRIndex
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const connection = await getDatabaseConnection()
-  console.log('connection');
+  
+  const posts = await connection.manager.find(Post)
+  console.log('posts', JSON.stringify(posts[0].createdAt), typeof posts[0].createdAt);
   
 
   const ua = context.req.headers['user-agent']
   const result = new UAParser(ua).getResult()
   return {
     props: {
-      browser: result.browser
+      browser: result.browser,
+      posts: JSON.parse(JSON.stringify(posts))
     }
   }
 }
