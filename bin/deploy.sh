720c9ca1b010094d5e5cee9718bc237e02d2a0d9
yojin <yojin1995@qq.com>
2020-10-29 22:44:58 +0800

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,10 @@
-echo Hello > /tmp/jin
+docker start blog &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build -t blog/node-web-app . &&
+docker kill blog &&
+docker rm blog &&
+docker run --network=host --name blog -p 3000:3000 -d blog/node-web-app &&
+echo 'Done!'
